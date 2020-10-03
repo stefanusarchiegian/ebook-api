b5db40dd35c1d99111a21276ea298d05ead77115
stefanusarchiegian <stefanusarchiegian@gmail.com>
2020-10-03 09:55:58 +0700

    authentication

--- routes/api.php
+++ routes/api.php
@@ -25,3 +25,14 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 //Route::delete("books/{id}", "BookController@destroy");
 
 Route::resource('books', 'BookController');
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+    Route::post('login', 'AuthController@login');
+    Route::post('register', 'AuthController@register');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::get('user-profile', 'AuthController@userProfile');
+});
